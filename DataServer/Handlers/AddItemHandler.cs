9364944afa160d3ab7feb758435127243e59f47c
Alan Potter <alan.potter@calnexsol.com>
2016-11-30 15:47:52 +0000

    Add missed files

---
+++ DataServer/Handlers/AddItemHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using DataServer.DataAccess.Interfaces;
+using DataObjects.RPCRequests;
+
+namespace DataServer.Handlers
+{
+    internal class AddItemHandler : IHandler
+    {
+        #region Declarations
+        private ITodoStorage _dataService;
+        #endregion Declarations
+
+        public AddItemHandler(ITodoStorage dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public string HandleRequest(string message)
+        {
+            var rpc = JsonConvert.DeserializeObject<RpcRequest<AddItemDto>>(message);
+
+            // At this stage, we return a query from the database
+            if(null != rpc)
+            {
+                var dto = rpc.Payload;
+                _dataService.AddTodoItem(dto.Key, dto.Description, dto.IsComplete, dto.TimeCreated);
+            }
+            else
+            {
+                throw new Exception("Cannot convert message to DTO");
+            }
+
+            return "";
+        }
+    }
+}
